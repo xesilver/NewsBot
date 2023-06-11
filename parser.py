7fd58ca00977750c576899843c09594f8c620644
xesilver <forwardgoldseries@gmail.com>
2023-06-11 19:03:13 +0300

    NewsBot

--- parser.py
+++ parser.py
@@ -1,67 +1,101 @@
-import requests
+import asyncio
+import aiohttp
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+import time
+
 
 URL = {
-    'Politico': 'https://www.politico.com/',
-    'StopGame': 'https://stopgame.ru/',
-    'IGN': 'https://www.ign.com/',
-    'Skysports': 'https://www.skysports.com/',
-    'Sportguardian': 'https://www.theguardian.com/uk/sport'
+    'politico': 'https://www.politico.com/',
+    'stopgame': 'https://stopgame.ru/',
+    'ign': 'https://www.ign.com/',
+    'skysports': 'https://www.skysports.com/',
+    'sportguardian': 'https://www.theguardian.com/uk/sport'
 }
-ua = UserAgent()
-headers = {'User-Agent': ua.chrome}
+
+headers = {'User-Agent': UserAgent().chrome}
 
 
-def parse_politico() -> list:
-    request = requests.get(URL.get('Politico')).text
-    soup = BeautifulSoup(request, 'lxml')
-
-    links = tuple(elem.get('href') for elem in soup.findAll('a',
-                                                            class_='js-tealium-tracking'))
-    articles = tuple(elem.text for elem in soup.findAll('a',
-                                                        class_='js-tealium-tracking'))
-    return [_ for _ in zip(articles, links)]
+async def fetch(session, url):
+    async with session.get(url, headers=headers) as response:
+        return await response.text()
 
 
-def parse_stopgame() -> list:
-    request = requests.get(URL.get('StopGame')).text
-    soup = BeautifulSoup(request, 'lxml')
+async def parse_politico():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, URL.get('politico'))
+        soup = BeautifulSoup(html, 'lxml')
 
-    links = tuple("https://stopgame.ru/" + elem.get('href') for elem in soup.findAll('a',
-                                                                                     class_='_news-widget__item_p8g9a_272'))
-    articles = tuple(elem.text.replace('\n', '').strip() for elem in soup.findAll('a',
-                                                                                  class_='_news-widget__item_p8g9a_272'))
-    return [_ for _ in zip(articles, links)]
+        links = tuple(elem.get('href') for elem in soup.findAll('a', class_='js-tealium-tracking'))
+        articles = tuple(elem.text for elem in soup.findAll('a', class_='js-tealium-tracking'))
 
-
-def parse_ign() -> list:
-    request = requests.get(URL.get('IGN'), headers=headers).text
-    soup = BeautifulSoup(request, 'lxml')
-
-    links = tuple(elem.get('src') for elem in soup.findAll('img',
-                                                           class_='jsx-2920405963 progressive-image jsx-1049729975 item-image aspect-ratio aspect-ratio-16-9 jsx-1330092051 jsx-3166191823 rounded hover-opacity'))
-    articles = tuple(elem.get('alt') for elem in soup.findAll('img',
-                                                              class_='jsx-2920405963 progressive-image jsx-1049729975 item-image aspect-ratio aspect-ratio-16-9 jsx-1330092051 jsx-3166191823 rounded hover-opacity'))
-    return [_ for _ in zip(articles, links)]
+        return [_ for _ in zip(articles, links)]
 
 
-def parse_guardiansport():
-    request = requests.get(URL.get('Sportguardian')).text
-    soup = BeautifulSoup(request, 'lxml')
+async def parse_stopgame():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, URL.get('stopgame'))
+        soup = BeautifulSoup(html, 'lxml')
 
-    articles = tuple(elem.text for elem in soup.findAll('a', class_='u-faux-block-link__overlay js-headline-text'))
-    links = tuple(elem.get('href') for elem in soup.findAll('a', class_='u-faux-block-link__overlay js-headline-text'))
+        links = tuple("https://stopgame.ru/" + elem.get('href') for elem in soup.findAll('a',
+                                                                                         class_='_news-widget__item_p8g9a_272'))
+        articles = tuple(elem.text.replace('n', '').strip() for elem in soup.findAll('a',
+                                                                                      class_='_news-widget__item_p8g9a_272'))
 
-    return [_ for _ in zip(articles, links)]
+        return [_ for _ in zip(articles, links)]
 
 
-def parse_skysports() -> list:
-    request = requests.get(URL.get('Skysports')).text
-    soup = BeautifulSoup(request, 'lxml')
+async def parse_ign():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, URL.get('ign'))
+        soup = BeautifulSoup(html, 'lxml')
 
-    links = tuple(elem.get('href') for elem in soup.findAll('a', class_='sdc-site-tile__headline-link'))
-    articles = tuple(elem.text for elem in soup.findAll('span', class_='sdc-site-tile__headline-text'))
-    sport_names = tuple(elem.text.strip() for elem in soup.findAll('a', class_='sdc-site-tile__tag-link'))
+        links = tuple(elem.get('src') for elem in soup.findAll('img',
+                                                               class_='jsx-2920405963 progressive-image jsx-1049729975 item-image aspect-ratio aspect-ratio-16-9 jsx-1330092051 jsx-3166191823 rounded hover-opacity'))
+        articles = tuple(elem.get('alt') for elem in soup.findAll('img',
+                                                                  class_='jsx-2920405963 progressive-image jsx-1049729975 item-image aspect-ratio aspect-ratio-16-9 jsx-1330092051 jsx-3166191823 rounded hover-opacity'))
 
-    return [_ for _ in zip(sport_names, articles, links)]
+        return [_ for _ in zip(articles, links)]
+
+
+async def parse_guardiansport():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, URL.get('sportguardian'))
+        soup = BeautifulSoup(html, 'lxml')
+
+        articles = tuple(elem.text for elem in soup.findAll('a', class_='u-faux-block-link__overlay js-headline-text'))
+        links = tuple(elem.get('href') for elem in soup.findAll('a', class_='u-faux-block-link__overlay js-headline-text'))
+
+        return [_ for _ in zip(articles, links)]
+
+
+async def parse_skysports():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, URL.get('skysports'))
+        soup = BeautifulSoup(html, 'lxml')
+
+        links = tuple(elem.get('href') for elem in soup.findAll('a', class_='sdc-site-tile__headline-link'))
+        articles = tuple(elem.text for elem in soup.findAll('span', class_='sdc-site-tile__headline-text'))
+        sport_names = tuple(elem.text.strip() for elem in soup.findAll('a', class_='sdc-site-tile__tag-link'))
+
+        return [_ for _ in zip(sport_names, articles, links)]
+
+
+async def main():
+    start = time.time()
+    tasks = [
+        asyncio.create_task(parse_skysports()),
+        asyncio.create_task(parse_guardiansport()),
+        asyncio.create_task(parse_ign()),
+        asyncio.create_task(parse_stopgame()),
+        asyncio.create_task(parse_politico())
+    ]
+
+    for result in await asyncio.gather(*tasks):
+        print(result)
+
+    end = time.time()
+    total_time = end - start
+    print(total_time)
+
+asyncio.run(main())
