19a38256f8f583559ebf016db85fda986762dd1d
xesilver <forwardgoldseries@gmail.com>
2023-06-11 20:41:25 +0300

    NewsBot

--- parser.py
+++ parser.py
@@ -4,13 +4,13 @@ from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import time
 
-
 URL = {
     'politico': 'https://www.politico.com/',
     'stopgame': 'https://stopgame.ru/',
     'ign': 'https://www.ign.com/',
     'skysports': 'https://www.skysports.com/',
-    'sportguardian': 'https://www.theguardian.com/uk/sport'
+    'sportguardian': 'https://www.theguardian.com/uk/sport',
+    'voanews': 'https://www.voanews.com/'
 }
 
 headers = {'User-Agent': UserAgent().chrome}
@@ -40,7 +40,7 @@ async def parse_stopgame():
         links = tuple("https://stopgame.ru/" + elem.get('href') for elem in soup.findAll('a',
                                                                                          class_='_news-widget__item_p8g9a_272'))
         articles = tuple(elem.text.replace('n', '').strip() for elem in soup.findAll('a',
-                                                                                      class_='_news-widget__item_p8g9a_272'))
+                                                                                     class_='_news-widget__item_p8g9a_272'))
 
         return [_ for _ in zip(articles, links)]
 
@@ -64,7 +64,8 @@ async def parse_guardiansport():
         soup = BeautifulSoup(html, 'lxml')
 
         articles = tuple(elem.text for elem in soup.findAll('a', class_='u-faux-block-link__overlay js-headline-text'))
-        links = tuple(elem.get('href') for elem in soup.findAll('a', class_='u-faux-block-link__overlay js-headline-text'))
+        links = tuple(
+            elem.get('href') for elem in soup.findAll('a', class_='u-faux-block-link__overlay js-headline-text'))
 
         return [_ for _ in zip(articles, links)]
 
@@ -81,6 +82,19 @@ async def parse_skysports():
         return [_ for _ in zip(sport_names, articles, links)]
 
 
+async def parse_voanews():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, URL.get('voanews'))
+        soup = BeautifulSoup(html, 'lxml')
+
+        links = tuple('https://www.voanews.com/' + elem.get('href') for elem in
+                      soup.findAll('a', class_='img-wrap img-wrap--t-spac img-wrap--size-4 img-wrap--float'))
+        articles = tuple(elem.text.replace('\n', '').strip() for elem in
+                         soup.findAll('h4', class_='media-block__title media-block__title--size-4'))
+
+        return [_ for _ in zip(articles, links)]
+
+
 async def main():
     start = time.time()
     tasks = [
@@ -88,7 +102,8 @@ async def main():
         asyncio.create_task(parse_guardiansport()),
         asyncio.create_task(parse_ign()),
         asyncio.create_task(parse_stopgame()),
-        asyncio.create_task(parse_politico())
+        asyncio.create_task(parse_politico()),
+        asyncio.create_task(parse_voanews())
     ]
 
     for result in await asyncio.gather(*tasks):
@@ -97,5 +112,6 @@ async def main():
     end = time.time()
     total_time = end - start
     print(total_time)
+
 
 asyncio.run(main())
